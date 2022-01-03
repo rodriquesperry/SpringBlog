e4f4559e0d434346d71786db5aaac963ec4d3f6f
Rodriques <rodriques.perry@gmail.com>
2022-01-03 12:29:14 -0600

    complete: HomeController and join.html

--- src/main/java/com/codeup/springblog/controller/HomeController.java
+++ src/main/java/com/codeup/springblog/controller/HomeController.java
@@ -1,7 +1,10 @@
 package com.codeup.springblog.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -15,5 +18,16 @@ public class HomeController {
     @GetMapping("/home")
     public String welcome() {
         return "home"; // Return should be the name of the html file
+    }
+
+    @GetMapping("/join")
+    public String showJoinForm() {
+        return "join";
+    }
+
+    @PostMapping("join")
+    public String joinCOhort(@RequestParam(name="cohort") String cohort, Model model) {
+        model.addAttribute("cohort", "Welcome to " + cohort + "!");
+        return "join";
     }
 }
