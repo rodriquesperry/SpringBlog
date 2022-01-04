81efeecbaacbe80e1ebbb0455cef3bfcb4d2b2f2
Rodriques <rodriques.perry@gmail.com>
2022-01-04 11:47:30 -0600

    complete: Repositories & JPA Exercises through #5

---
+++ src/main/java/com/codeup/springblog/daos/PostRepository.java
@@ -0,0 +1,7 @@
+package com.codeup.springblog.daos;
+
+import com.codeup.springblog.models.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+}
